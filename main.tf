db562fb7640b8b2595374ad687e846e59f574666
bandithak <73412582+bandithak@users.noreply.github.com>
2021-03-14 17:26:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,18 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
+}
+
+resource "aws_lightsail_instance" "Valheim-Smior" {
+  name              = "Valheim-Smior"
+  availability_zone = "us-west-2"
+  blueprint_id      = "string"
+  bundle_id         = "string"
+  key_pair_name     = "default"
+  user_data         = "./Valhalla.sh"
+  tags = {
+    foo = "Valheim-Smior"
+  }
 }
